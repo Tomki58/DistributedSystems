b4bd0b5d364be37b0cab8969e874b891f910b499
Tomki58 <tomki589@gmail.com>
2020-12-24 08:54:08 +0300

    lab_2 finished

--- lab_2.py
+++ lab_2.py
@@ -1,10 +1,5 @@
 from itertools import chain
 
-# graph = {'0': {'in': ['1'], 'out': ['2']}, '1': {'in': ['3'], 'out': ['0', '2']},
-#         '2': {'in': ['0', '1'], 'out': ['4']}, '3': {'in': ['4'], 'out': ['1']},
-#         '4': {'in': ['2'], 'out': ['3']}}
-
-
 graph = {
     "1": {"in": ["4"], "out": ["2", "3"]},
     "2": {"in": ["1"], "out": ["4"]},
@@ -21,16 +16,26 @@ class Site:
         self.ninc = set()
         self.inp = dict()
 
-    def check_for_change(self, inc_message, ninc_message):
-        new_inc = set.union(self.inc, inc_message)
-        new_ninc = set.union(self.ninc, ninc_message)
+    def check_for_change(self, prev_site):
+        new_inc = set.union(self.inc, prev_site.inc)
+        new_ninc = set.union(self.ninc, prev_site.ninc)
+
+        self.inp[prev_site.ID] = True
 
         if (len(new_inc) != len(self.inc)) or (len(new_ninc) != len(self.ninc)):
             self.inc = new_inc
             self.ninc = new_ninc
             return True
-        
+
         return False
+
+    def check_if_decided(self):
+        return True if self.inc == self.ninc else False
+
+    def check_neighbours(self):
+        if all(self.inp.values()):
+            self.ninc.add(self.ID)
+            self.inp = {k: False for k in self.inp.keys()}
 
 
 def parse_graph(graph: dict):
@@ -43,24 +48,42 @@ def parse_graph(graph: dict):
         tmp_site.inp = {k: False for k in graph[site_id]["in"]}
         sites.append(tmp_site)
 
+    return sites
+
 
 def find_by_id(sites, ID):
     return next((site for site in sites if site.ID == ID), None)
 
 
 def finn_algorithm(sites, init_site_id: str):
-    init_site = find_by_id(
-        sites, init_site_id
-    )
+    init_site = find_by_id(sites, init_site_id)
     init_flag = True
     site_queue = iter([init_site])
     current_site = next(site_queue)
     while True:
         # TODO: Сделать заполнение очереди через изменение сайта
-        pass
+        current_site.check_neighbours()
+        site_queue = chain(
+            iter(
+                [
+                    find_by_id(sites, out_id)
+                    for out_id in current_site.out
+                    if find_by_id(sites, out_id).check_for_change(current_site)
+                ]
+            ),
+            site_queue,
+        )
+
+        if current_site.check_if_decided():
+            print("Site %s decided!" % current_site.ID)
+            break
+        else:
+            current_site = next(site_queue)
+
     pass
 
 
 if __name__ == "__main__":
 
-    parse_graph(graph)
+    sites = parse_graph(graph)
+    finn_algorithm(sites, "1")
